de1b5331e4b61259a4f9fb9613c5ccf94df6860c
Shelcy-Calderon <shelcyjulianacalderon@gmail.com>
2022-01-11 08:26:43 -0500

    :lipstick: [ADD] Agregando estilos css

--- parte1/hola/urls.py
+++ parte1/hola/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns =[
     path('', views.hola, name='hola'),
-    path('vader/', views.vader, name='vader'),
-    path('starwars', views.starwars, name='starwars'),
-    path('<str:nombre>/', views.saludo, name='saludo'),
+    # path('vader/', views.vader, name='vader'),
+    # path('starwars', views.starwars, name='starwars'),
+    # path('<str:nombre>/', views.saludo, name='saludo'),
+    path('moneda/', views.moneda, name='moneda')
+
 ]
--- parte1/hola/views.py
+++ parte1/hola/views.py
@@ -1,3 +1,4 @@
+from typing import ContextManager
 from django.http.response import HttpResponse
 from django.shortcuts import render
 # from django.http import HttpResponse
@@ -5,16 +6,27 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def moneda(request):
+    num = 1
+    context = {'num' : num}
+    return render(request, 'hola/moneda.html', context)
+
 # ============ Estaticas ============
 def hola(request):
-    return HttpResponse("Hola, mundo")
+    return render(request, 'hola/index.html')
 
-def vader(request):
-    return HttpResponse("Hola, Darth Vader")
+# def vader(request):
+#     return HttpResponse("Hola, Darth Vader")
 
-def starwars(request):
-    return HttpResponse("Hola StartWars")
+# def starwars(request):
+#     return HttpResponse("Hola StartWars")
 
 # ============ Dinamicas ============
+# def saludo(request, nombre):
+#     return HttpResponse(f"Hola {nombre}!!!")
+
 def saludo(request, nombre):
-    return HttpResponse(f"Hola {nombre}!!!")
+    context = {'name': nombre}
+    return render(request, 'hola/saludo.html', context)
+
+
